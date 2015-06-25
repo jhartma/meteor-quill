769422a0f556f858288e4a2016aa9ceeb7ed8477
Denis Andrejew <da.colonel@gmail.com>
2015-06-25 14:25:40 +0100

    updating to quilljs 0.19.14

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'jhartma:quill',
-  version: '0.0.2',
+  name: 'themeteorites:quilljs',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
-  summary: 'Quill.js version 0.19.8 for Meteor',
+  summary: 'Quill.js version 0.19.14 for Meteor',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/jhartma/meteor-quill',
+  git: 'https://github.com/themeteorites/quilljs',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -12,8 +12,6 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
-
-  api.use('coffeescript')
 
   api.export('Quill','client');
   api.addFiles('quill/quill.js','client');
