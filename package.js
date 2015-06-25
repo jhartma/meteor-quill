4a9255903c1d09988ab8b92bbab15bd8d89da5d8
Denis Andrejew <da.colonel@gmail.com>
2015-06-25 14:33:21 +0100

    minor updates

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'themeteorites:quilljs',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'Quill.js version 0.19.14 for Meteor',
   // URL to the Git repository containing the source code for this package.
@@ -13,9 +13,9 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
 
-  api.export('Quill','client');
-  api.addFiles('quill/quill.js','client');
-  api.addFiles('quill.js','client');
+  api.export('Quill', 'client');
+  api.addFiles('quill/quill.js', 'client');
+  api.addFiles('export.js', 'client');
   api.addFiles('quill/quill.base.css', 'client');
   api.addFiles('quill/quill.snow.css', 'client');
 });
